148e0e7396cc9a2dbe5aef4d767a85d0d8798093
mkou <m.laitinen@elisanet.fi>
2017-01-19 13:21:06 +0200

    Aloittelua

---
+++ Jmtetra/src/main/java/jmtetra/jmtetra/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jmtetra.jmtetra;
+
+/**
+ *
+ * @author mikko
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+    
+    public static String hello() {
+        return "hello";
+    }
+}
